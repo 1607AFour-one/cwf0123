72b9a71d552a5d16ca7b74f796d4b8e59f7ea006
ZhaoTengShuai <45376004+ZhaoTengShuai@users.noreply.github.com>
2019-01-23 10:01:41 +0800

    第二次

--- app/src/main/java/com/bawei/chewenfei/myapplication/MainActivity.java
+++ app/src/main/java/com/bawei/chewenfei/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //my first commit
+        //第二次提交
     }
 }
